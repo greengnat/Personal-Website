a92630419662c6323c28022f189f1e43844c5148
Will <will.lucas1996@gmail.com>
2020-08-24 15:30:08 -0700

    creating project page

--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -1,5 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+import styled from "styled-components";
+
+const Display = styled.div`
+  display: flex;
+  width: 100vw;
+  margin: 0 auto;
+  padding: 0;
+`;
+
+const SideBar = styled.div`
+  width: 15vw;
+  height: 100vh;
+  background-color: grey;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+`;
+
+const ProjectContainer = styled.div`
+  width: 85vw;
+  height: 100vh;
+  background-color: lightblue;
+`;
 
 export default function Projects() {
-  return <div>hi</div>;
+  return (
+    <Display>
+      <SideBar></SideBar>
+      <ProjectContainer></ProjectContainer>
+    </Display>
+  );
 }
